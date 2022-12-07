0fada18e3ccdb0cdc881b298cf97e9497ab13b59
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2022-12-07 13:44:10 -0500

    Created apiroutes.js file

---
+++ Main/routes/apiRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').router();
+const store = require('../db/store');
+
+// Responds with all notes in the database
+router.get('/notes', (req, res) => {
+    store
+      .getNotes()
+      .then((notes) => {
+        return res.json(notes);
+      })
+      .catch((err) => res.status(500).json(err));
+});
+
+router.post('/notes', (req, res) => {
+    store
+      .addNote(req.body)
+      .then((note) => res.json(note))
+      .catch((err) => res.status(500).json(err));
+});
+
+// Deletes the note that has an id equal to req.params.id
+router.delete('/notes/:id', (req, res) => {
+    store
+      .removeNote(req.params.id)
+      .then(() => res.json({ ok: true }))
+      .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
